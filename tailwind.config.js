a5b49f17a81f805f104f3e6b521ef03fdddc2712
Jonathan S. Ruedas <jonathan.ruedas@microsourcing.ph>
2024-06-22 22:58:04 +0800

    this commit will add homepage layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './**/*.liquid'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./**/*.liquid'],
+    theme: {
+        fontFamily: {
+            manrope: ['Manrope', 'sans-serif'],
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1.5rem',
+                sm: '2rem',
+                lg: '3.5rem',
+                xl: '8rem',
+                '2xl': '10rem',
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
 }
-
